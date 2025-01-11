113f8dfef058a8674af2d94fae3786587f8d0f53
lh64 <lh64@cornell.edu>
2025-01-11 13:17:13 -0600

    Added simulation commands to cellulose.py and created the data file for running PyDREAM. Also, adjusted the rate constants to produce output that more closely matches the data

--- cellulose.py
+++ cellulose.py
@@ -15,30 +15,31 @@ Monomer('FA')
 Monomer('LA')
 
 # Initial conditions
-Parameter('Cellulose_0', 0.5)
+Parameter('Cellulose_0', 50) # 0.5
 Initial(Cellulose(), Cellulose_0)
 
 # Rules
-Parameter('k1', 0.211)
-Rule('Cellulose_to_Glucose', Cellulose() >> Cellulose() + Glucose(), k1)
+Parameter('k1', 0.01) # 0.211)
+# Rule('Cellulose_to_Glucose', Cellulose() >> Cellulose() + Glucose(), k1)
+Rule('Cellulose_to_Glucose', Cellulose() >> Glucose(), k1)
 
-Parameter('k2', 0.143)
+Parameter('k2', 0.01) # 0.143)
 Rule('Glucose_to_HMF', Glucose() >> HMF(), k2)
 
-Parameter('k3', 1.651)
+Parameter('k3', 0.01) # 1.651)
 Rule('HMF_to_LA_FA', HMF() >> LA() + FA(), k3)
 
-Parameter('k4', 1.870)
+Parameter('k4', 0.1) # 1.870)
 Rule('Glucose_to_Furfural_FA', Glucose() >> Furfural() + FA(), k4)
 
-Parameter('k5', 1.533)
+Parameter('k5', 0.001) # 1.533)
 Rule('Furfural_to_LA', Furfural() >> LA(), k5)
 
-Parameter('kf6', 0.350)
-Parameter('kr6', 0.349)
+Parameter('kf6', 0.1) # 0.350)
+Parameter('kr6', 0.1) # 0.349)
 Rule('Glucose_to_Fructose', Glucose() | Fructose(), kf6, kr6)
 
-Parameter('k7', 1.810)
+Parameter('k7', 0.1) # 1.810)
 Rule('Fructose_to_HMF', Fructose() >> HMF, k7)
 
 # Observables
@@ -49,22 +50,34 @@ Observable('Furfural_Obs', Furfural())
 Observable('FA_Obs', FA())
 Observable('LA_Obs', LA())
 
-# Simulation commands
+if __name__ == '__main__':
 
-# tspan = np.linspace(0, 16*60, 16*60*10+1)
-tspan = np.linspace(0, 150, 1501)
-sim = ScipyOdeSimulator(model, tspan, verbose=True)
-output = sim.run()
+    # experimental data
+    data = np.genfromtxt('cellulose_data_160C.csv', dtype=None, delimiter=',', names=True, encoding="utf_8_sig")
+    exp_observables = np.unique([d['observable'] for d in data])
 
-for obs in model.observables:
-    plt.plot(tspan, output.observables[obs.name], lw=2, label=obs.name)
-plt.xlabel('Time (min)')
-plt.ylabel('Concentration (mM)')
-plt.legend(loc='best')
-plt.tight_layout()
+    # Simulation commands
+    tspan = np.linspace(0, 16*60, 16*60*10+1)
+    # tspan = np.linspace(0, 150, 1501)
+    sim = ScipyOdeSimulator(model, tspan, verbose=True)
+    output = sim.run()
 
-# ODEs
-for i, ode in enumerate(model.odes):
-    print('%s:' % model.species[i], ode)
+    plt.figure(constrained_layout=True, figsize=(6.4*1.2, 4.8))
+    for obs in model.observables:
+        # plot simulation result
+        p = plt.plot(tspan, output.observables[obs.name], lw=2, label=obs.name)
+        if obs.name in exp_observables:
+            # plot experimental data
+            t_pts = [d['time'] * 60 for d in data if d['observable'] == obs.name] # convert from hrs to minutes
+            avg = [d['average'] for d in data if d['observable'] == obs.name]
+            stderr = [d['stderr'] for d in data if d['observable'] == obs.name]
+            plt.errorbar(t_pts, avg, stderr, fmt='o', color=p[0].get_color(), capsize=6)
+    plt.xlabel('Time (min)')
+    plt.ylabel('Concentration (mM)')
+    plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
 
-plt.show()
+    # ODEs
+    for i, ode in enumerate(model.odes):
+        print('%s:' % model.species[i], ode)
+
+    plt.show()
